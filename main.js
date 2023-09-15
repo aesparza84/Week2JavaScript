f5b90efdfb71b9474df459f5f534e6612d9e2220
aesparza84 <aesparza@colum.edu>
2023-09-14 22:13:27 -0500

    added index and main

---
+++ main.js
@@ -0,0 +1,13 @@
+const firstNum = document.getElementById('input1');
+const secondNum = document.getElementById('input2');
+const outBox = document.getElementById('output');
+const btn = document.getElementById('butn');
+
+console.log(btn);
+console.log(firstNum);
+
+
+function ComputSumAndRoot()
+{
+    outBox.innerHTML = firstNum;
+}
