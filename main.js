cf6434125e1c801fd47a6fe631feb72ff1552367
aesparza84 <aesparza@colum.edu>
2023-09-15 00:04:28 -0500

    can read inputs and do the logic

--- main.js
+++ main.js
@@ -1,12 +1,29 @@
-const firstNum = document.getElementById('input1');
-const secondNum = document.getElementById('input2');
-const outBox = document.getElementById('output');
-const btn = document.getElementById('butn');
+//first number
+//let firstNum = document.getElementById('input1').setAttribute('type','number');
 
-console.log(firstNum);
-console.log(outBox);
+//second number
+
+
+//output box
+let outBox = document.getElementById('output');
+
+//The button
+let btn = document.getElementById('butn');
 
 function ComputSumAndRoot()
 {
-    outBox.innerHTML = firstNum;
+    let firstNum = Number(document.getElementById('input1').value);
+    let secondNum = Number(document.getElementById('input2').value);
+    console.log(firstNum +" is a "+typeof(firstNum));
+    console.log(secondNum +" is a "+typeof(firstNum));
+
+    let firstRoot = Math.sqrt(firstNum);
+    let secondRoot = Math.sqrt(secondNum);
+    let sumOfBoth = firstNum + secondNum;
+
+    console.log("Sum: "+sumOfBoth);
+    console.log("Product of roots "+ firstRoot*secondRoot);
+    outBox.innerHTML = "Sum:              "+sumOfBoth+"\n"+"Product of roots: "+firstRoot*secondRoot;
 }
+
+btn.addEventListener('click', ComputSumAndRoot);
