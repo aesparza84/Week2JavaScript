2ce2233b369770a2ecaeaf7580114751d7873cb1
aesparza84 <aesparza@colum.edu>
2023-09-16 00:16:50 -0500

    Added more to CSS

--- main.js
+++ main.js
@@ -1,21 +1,18 @@
-//first number
-//let firstNum = document.getElementById('input1').setAttribute('type','number');
-
-//second number
-
 
 //output box
-let outBox = document.getElementById('output');
+let sumBox = document.getElementById('outputSum');
+let rootBox = document.getElementById('outputRoot');
 
 //The button
 let btn = document.getElementById('butn');
 
 function ComputSumAndRoot()
 {
+    //First Number
     let firstNum = Number(document.getElementById('input1').value);
+
+    //Second number
     let secondNum = Number(document.getElementById('input2').value);
-    console.log(firstNum +" is a "+typeof(firstNum));
-    console.log(secondNum +" is a "+typeof(firstNum));
 
     let firstRoot = Math.sqrt(firstNum);
     let secondRoot = Math.sqrt(secondNum);
@@ -23,7 +20,22 @@ function ComputSumAndRoot()
 
     console.log("Sum: "+sumOfBoth);
     console.log("Product of roots "+ firstRoot*secondRoot);
-    outBox.innerHTML = "Sum:              "+sumOfBoth+"\n"+"Product of roots: "+firstRoot*secondRoot;
+    sumBox.innerHTML= "Sum: "+sumOfBoth;
+
+    //Checks if the inputs are negative or not for Square Root
+    if(firstNum < 0 || secondNum < 0)
+    {
+        rootBox.innerHTML = "Can't take root of negatives";
+    }
+    else
+    {
+        //Gives the squre root, up to 2 deciaml places
+        rootBox.innerHTML = "Product of roots: "+(firstRoot*secondRoot).toFixed(2);
+    }
+
+    //Debug purposes
+    console.log(firstNum +" is a "+typeof(firstNum));
+    console.log(secondNum +" is a "+typeof(firstNum));    
 }
 
 btn.addEventListener('click', ComputSumAndRoot);
